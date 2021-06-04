2d2a344d2559ca8bc4a07082247faccc995ce2b3
Linh31 <nguyentuanlinh311@gmail.com>
2021-06-04 19:40:30 +0700

    baithi

--- src/Baithi/Hotel.java
+++ src/Baithi/Hotel.java
@@ -48,6 +48,7 @@ public class Hotel {
     public static void main(String[] args) {
         Hotel h = new Hotel();
         Scanner sc = new Scanner(System.in);
+//        int choice = 0;
         int count = 0;
         h.insertHotel("maivilla","hanoi","linh");
         h.insertHotel("songhong","ho chi minh","long");
@@ -60,16 +61,40 @@ public class Hotel {
         h.insertHotel("Lasiesta","hoi an","chi");
         h.insertHotel("Allegro","hoi an","huong");
 
+//        do{
+//            System.out.println("------Menu------");
+//            System.out.println("------1.nhap vao danh sach hotel------");
+//            System.out.println("------2.tim kiem hotel theo ten chu so huu------");
+//            System.out.println("----End Menu-----");
+//
+//            System.out.println("nhap vao lua chon: ");
+//            choice = sc.nextInt();
+//            switch (choice){
+//                case 1:
+//                    truong hop cho quan ly nhap
+//                    System.out.println("nhap vao ten hotel: ");
+//                    sc.nextLine();
+//                    String name = sc.nextLine();
+//                    System.out.println("nhap vao dia diem: ");
+//                    String location = sc.nextLine();
+//                    System.out.println("nhap vao chu so huu: ");
+//                    String ownername = sc.nextLine();
+//                    h.insertHotel(name,location,ownername);
+//                    break;
+//                case 2:
+//                    System.out.println("nhap vao ten chu so huu khach san: ");
+//                    sc.nextLine();
+//                    String own = sc.nextLine();
+//                    h.displayDetails(own);
+//                    break;
+//                default:
+//                    System.out.println("invalid choice");
+//            }
+//        }while(choice !=3);
+
         do{
-            
-   //truong hop cho quan ly nhap
-//            System.out.println("nhap vao ten hotel: ");
-//            String name = sc.nextLine();
-//            System.out.println("nhap vao dia diem: ");
-//            String location = sc.nextLine();
-//            System.out.println("nhap vao chu so huu: ");
-//            String ownername = sc.nextLine();
-//            h.insertHotel(name,location,ownername);
+
+
 
             count++;
 
